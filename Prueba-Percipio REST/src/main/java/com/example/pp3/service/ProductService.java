4ea48744c31d1bd4f3adcf0d490a5cc384765a31
jukaraider <a788782@10.85.162.215>
2021-01-26 16:52:40 -0300

    Renombro la carpeta

--- Prueba-Percipio REST/src/main/java/com/example/pp3/service/ProductService.java
+++ Prueba-Percipio REST/src/main/java/com/example/pp3/service/ProductService.java
@@ -3,7 +3,6 @@ package com.example.pp3.service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
